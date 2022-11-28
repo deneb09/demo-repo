f19acb8e285188cd68a01f62e642837a156761aa
deneb09 <104804393+deneb09@users.noreply.github.com>
2022-11-29 07:15:54 +0800

    Update Deployment script.ps1

--- Deployment script.ps1
+++ Deployment script.ps1
@@ -1,2 +1,1 @@
-﻿$workbooks = Invoke-WebRequest https://api.github.com/repos/helderpinto/AzureOptimizationEngine/contents/views/workbooks
-$workbooksjson = ($workbooks | ConvertFrom-Json ).download_url | Select-String -SimpleMatch "-arm.json"
+New-AzManagementGroupDeployment -Name CustomerOnboardviaPolicy -Location AustraliaEast -ManagementGroupId 4005dec5-fb4a-48a2-9fd8-b22d223db569 -TemplateFile "C:\Users\akumar3\OneDrive - Insight\Desktop\Lighthouse templates\Lighthouse Policy\deployLighthouseIfNotExistManagementGroup.json" -TemplateParameterFile "C:\Users\akumar3\OneDrive - Insight\Desktop\Lighthouse templates\Lighthouse Policy\deployLighthouseIfNotExistsManagementGroup.parameters.json" -verbose
