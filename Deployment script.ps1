e56a7c88ae580bdf13d722bd45a4436d01d81608
deneb09 <104804393+deneb09@users.noreply.github.com>
2022-11-29 07:28:43 +0800

    Delete Deployment script.ps1

--- Deployment script.ps1
+++
@@ -1,1 +0,0 @@
-New-AzManagementGroupDeployment -Name CustomerOnboardviaPolicy -Location AustraliaEast -ManagementGroupId 9342bb13-ba90-48aa-b2f8-084d0a105c33 -TemplateFile .\deployLighthouseIfNotExistManagementGroup.json -TemplateParameterFile .\deployLighthouseIfNotExistsManagementGroup.parameters.json -verbose
